93bc7a407cca476f6fbbb2eeef6492562d327a12
fnzl54 <rnjscksdud1@naver.com>
2023-08-26 15:54:41 +0900

    feat: 기본 파일 commit

---
+++ src/main/kotlin/com/techeer/goodnighthackathon/GoodNightHackathonApplication.kt
@@ -0,0 +1,11 @@
+package com.techeer.goodnighthackathon
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GoodNightHackathonApplication
+
+fun main(args: Array<String>) {
+	runApplication<GoodNightHackathonApplication>(*args)
+}
---
+++ src/test/kotlin/com/techeer/goodnighthackathon/GoodNightHackathonApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.techeer.goodnighthackathon
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GoodNightHackathonApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
